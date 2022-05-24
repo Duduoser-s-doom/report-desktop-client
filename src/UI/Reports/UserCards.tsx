8e6e52caa5904a6decdec19cceb5eda409ffa041
XtereO <71135845+XtereO@users.noreply.github.com>
2022-05-24 20:24:01 +0300

    loader added

--- src/UI/Reports/UserCards.tsx
+++ src/UI/Reports/UserCards.tsx
@@ -1,13 +1,14 @@
 import { observer } from "mobx-react";
 import { reports } from "../../BLL/Reports";
 import { Pagination, UserCard } from "../Bricks";
+import loading from "../assets/loading.gif"
 
 export const UserCards = observer(() => {
   return (
     <div id="usercards" className="my-2">
-      <div style={{ height: 450, overflowY: "scroll" }}>
+      <div style={{ height: 450, overflowY: "scroll",display: "flex", justifyContent:"center" }}>
         {reports.isFetchingReports
-          ? "Loading"
+          ? <img src={loading} />
           : reports.reports.fromSearch.map((r) => (
               <div key={`usercard${r.reportId}`} className="my-2">
                 <UserCard {...r} />
